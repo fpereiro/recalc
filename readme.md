858848d14b54678bba4e70825fb9918324ad5cfc
Federico Pereiro <fpereiro@gmail.com>
2015-10-22 11:34:14 -0300

    First commit.

---
+++ readme.md
@@ -0,0 +1,7 @@
+# recalc
+
+> "[T]hus, the external world would only have a triggering role in the release of the internally-determined activity of the nervous system." --Humberto Maturana
+
+recalc is a library for binding events to data changes. The core idea is to use side effects to trigger all code paths, in a systematic way.
+
+recalc is very much a work in progress. Feedback is very welcome (fpereiro@gmail.com).
---
+++ recalc.js
@@ -0,0 +1,92 @@
+/*
+recalc - v0.1.0
+
+Written by Federico Pereiro (fpereiro@gmail.com) and released into the public domain.
+
+Please refer to readme.md to read the annotated source.
+*/
+
+(function () {
+
+   // *** SETUP ***
+
+   var isNode = typeof exports === 'object';
+
+   var dale   = isNode ? require ('dale')   : window.dale;
+   var teishi = isNode ? require ('teishi') : window.teishi;
+
+   var main = function (data, routes) {
+      if (teishi.stop ([
+         ['data', data, ['array', 'object'], 'oneOf'],
+         ['routes', routes, 'array']
+      ])) return;
+
+      var pick = function (path) {
+         var output = data;
+         return dale.stopOn (path, false, function (v, k) {
+            if (k + 1 < path.length && teishi.simple (output [v])) return false;
+            output = output [v];
+         }) === false ? undefined : output;
+      }
+
+      var r = function (path, value) {
+         if (teishi.stop ('r', [
+            ['path', path, 'array'],
+            ['path', path, ['number', 'string'], 'eachOf'],
+         ])) return;
+
+         var result = pick (path);
+         if (result === value) return;
+         r.set (path, data, value);
+         dale.do (r.match (path, routes), function (v) {
+            v (path, value, result);
+         });
+      }
+
+      r.pick = pick;
+
+      r.set = function (path, data, value) {
+         if (teishi.stop ('r.set', [
+            ['path', path, 'array'],
+            ['path', path, ['number', 'string'], 'eachOf'],
+         ])) return;
+
+         var item = data;
+
+         dale.do (path, function (v, k) {
+            if (k + 1 === path.length) item [v] = value;
+            else                       item = item [v];
+         });
+      }
+
+      r.path = function (path) {
+         if (teishi.stop ('r.path', ['path', path, 'array'])) return;
+         return path.join ('-');
+      }
+
+      r.match = function (path, routes) {
+         var output = [];
+         dale.do (routes, function (v) {
+            dale.stopOn (v [0], false, function (v2, k2) {
+               if (path [k2] !== undefined && path [k2] !== '*' && v2 !== '*' && path [k2] !== v2) {
+                  return false;
+               }
+               if (k2 === v [0].length - 1) output.push (v [1]);
+            });
+         });
+         return output;
+      }
+
+      r.do = function (path) {
+         var fun = r.pick (path);
+         if (teishi.stop ('r.do', ['fun', fun, 'function'])) return;
+         fun.apply (undefined, teishi.c (arguments).slice (1));
+      }
+
+      return r;
+   }
+
+   if (isNode) return main;
+   else        window.R = main;
+
+}) ();
